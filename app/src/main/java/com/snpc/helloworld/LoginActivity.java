9e0c2e663dc98f28de09ad5c89e3b9025831ef46
snpcspam6789@gmail.com <snpcspam6789@gmail.com>
2017-12-20 00:50:28 +0700

    Commit Change

---
+++ app/src/main/java/com/snpc/helloworld/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.snpc.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Fly-monster on 20/12/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+}
---
+++ app/src/main/java/com/snpc/helloworld/Student.java
@@ -0,0 +1,8 @@
+package com.snpc.helloworld;
+
+/**
+ * Created by Fly-monster on 20/12/2017.
+ */
+
+public class Student {
+}
